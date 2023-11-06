f47aa6c0b9b664b806c4544fb6c872cdf846817e
Chocolate_Q <523759909@qq.com>
2023-11-06 13:58:54 +0800

    Update comm.py

--- FedDGP/alg/core/comm.py
+++ FedDGP/alg/core/comm.py
@@ -3,6 +3,8 @@
 
 import torch
 import copy
+import torch.nn as nn
+import torch.nn.functional as F
 
 def communication(args, server_model, models, client_weights):
     client_num=len(models)
@@ -29,6 +31,27 @@ def communication(args, server_model, models, client_weights):
                         server_model.state_dict()[key].data.copy_(temp)
                         if 'bn' not in key:
                             models[cl].state_dict()[key].data.copy_(server_model.state_dict()[key])
+        elif args.alg.lower() == 'fedsim':
+            for key in server_model.state_dict().keys():
+                if 'num_batches_tracked' in key:
+                    server_model.state_dict()[key].data.copy_(models[0].state_dict()[key])
+                else:
+                    temp_avg = torch.zeros_like(server_model.state_dict()[key])
+                    temp = torch.zeros_like(server_model.state_dict()[key])
+                    for client_idx in range(len(client_weights)):
+                        temp_avg += client_weights[client_idx] * models[client_idx].state_dict()[key]
+                    cosim_list = [F.cosine_similarity(torch.flatten(models[client_idx].state_dict()[key]), torch.flatten(temp_avg), dim=0)
+                                                      for client_idx in range(len(client_weights))]
+                    cosim_list = torch.stack(cosim_list)
+                    softmax = nn.Softmax(0)
+                    client_new_weights = softmax(cosim_list)
+                    for client_idx in range(len(client_new_weights)):
+                        temp += client_new_weights[client_idx] * models[client_idx].state_dict()[key]
+
+                    server_model.state_dict()[key].data.copy_(temp)
+
+                    for client_idx in range(len(client_weights)):
+                        models[client_idx].state_dict()[key].data.copy_(server_model.state_dict()[key])
         else:
             for key in server_model.state_dict().keys():
                 if 'num_batches_tracked' in key:
@@ -41,4 +64,65 @@ def communication(args, server_model, models, client_weights):
                     for client_idx in range(len(client_weights)):
                         models[client_idx].state_dict()[key].data.copy_(server_model.state_dict()[key])
     return server_model, models
-    
+
+
+def communication_cycle(args, server_model, models, client_weights, round):
+    for key in server_model.state_dict().keys():
+        if 'num_batches_tracked' in key:
+            server_model.state_dict()[key].data.copy_(models[0].state_dict()[key])
+        else:
+            if round<args.cround:
+                temp = models[round%args.n_clients].state_dict()[key]
+                server_model.state_dict()[key].data.copy_(temp)
+                if 'bn' not in key:
+                    for client_idx in range(len(client_weights)):
+                        models[client_idx].state_dict()[key].data.copy_(server_model.state_dict()[key])
+            else:
+                temp_avg = torch.zeros_like(server_model.state_dict()[key])
+                temp = torch.zeros_like(server_model.state_dict()[key])
+                for client_idx in range(len(client_weights)):
+                    temp_avg += client_weights[client_idx] * models[client_idx].state_dict()[key]
+                cosim_list = [F.cosine_similarity(torch.flatten(models[client_idx].state_dict()[key]), torch.flatten(temp_avg),dim=0)
+                                for client_idx in range(len(client_weights))]
+                cosim_list = torch.stack(cosim_list)
+                softmax = nn.Softmax(0)
+                client_new_weights = softmax(cosim_list)
+                for client_idx in range(len(client_new_weights)):
+                    temp += client_new_weights[client_idx] * models[client_idx].state_dict()[key]
+
+                server_model.state_dict()[key].data.copy_(temp)
+                if 'bn' not in key:
+                    for client_idx in range(len(client_weights)):
+                        models[client_idx].state_dict()[key].data.copy_(server_model.state_dict()[key])
+
+    return server_model, models
+
+def communication_cycle_nbn(args, server_model, models, client_weights, round):
+    for key in server_model.state_dict().keys():
+        if 'num_batches_tracked' in key:
+            server_model.state_dict()[key].data.copy_(models[0].state_dict()[key])
+        else:
+            if round<args.cround:
+                temp = models[round%args.n_clients].state_dict()[key]
+                server_model.state_dict()[key].data.copy_(temp)
+                for client_idx in range(len(client_weights)):
+                        models[client_idx].state_dict()[key].data.copy_(server_model.state_dict()[key])
+            else:
+                temp_avg = torch.zeros_like(server_model.state_dict()[key])
+                temp = torch.zeros_like(server_model.state_dict()[key])
+                for client_idx in range(len(client_weights)):
+                    temp_avg += client_weights[client_idx] * models[client_idx].state_dict()[key]
+                cosim_list = [F.cosine_similarity(torch.flatten(models[client_idx].state_dict()[key]), torch.flatten(temp_avg),dim=0)
+                                for client_idx in range(len(client_weights))]
+                cosim_list = torch.stack(cosim_list)
+                softmax = nn.Softmax(0)
+                client_new_weights = softmax(cosim_list)
+                for client_idx in range(len(client_new_weights)):
+                    temp += client_new_weights[client_idx] * models[client_idx].state_dict()[key]
+
+                server_model.state_dict()[key].data.copy_(temp)
+                for client_idx in range(len(client_weights)):
+                        models[client_idx].state_dict()[key].data.copy_(server_model.state_dict()[key])
+
+    return server_model, models
+
